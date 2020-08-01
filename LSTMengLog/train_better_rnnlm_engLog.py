5ef323750aae51e5183a6a23352f448292828d0f
KojiFujii <40759843+KojiFujii@users.noreply.github.com>
2020-08-01 18:43:38 +0900

    LSTMによるEngLog学習の環境整備

---
+++ LSTMengLog/train_better_rnnlm_engLog.py
@@ -0,0 +1,83 @@
+# coding: utf-8
+import sys
+sys.path.append('..')
+from common import config
+# GPUで実行する場合は下記のコメントアウトを消去（要cupy）
+# ==============================================
+config.GPU = True
+# ==============================================
+from common.optimizer import SGD
+from common.trainer import RnnlmTrainer
+from common.util import eval_perplexity, to_gpu
+from dataset import ptb
+from better_rnnlm import BetterRnnlm
+
+
+# ハイパーパラメータの設定
+batch_size = 20
+#wordvec_size = 650
+wordvec_size = 530
+#hidden_size = 650
+hidden_size = 530
+time_size = 35
+lr = 20.0
+max_epoch = 20
+max_grad = 0.25
+dropout = 0.5
+
+# 学習データの読み込み
+with open("2V6Corpus_print.pkl",  mode='rb') as file:
+    corpus = np.array(pickle.load(file))
+file.close()
+
+with open("2V6LogDictionary.pkl",  mode='rb') as file:
+    dictionary = np.array(pickle.load(file))
+file.close()
+
+corpus_size = len(corpus)
+corpusMaster = corpus
+corpus = corpus[:(corpus_size * 70 / 100)]
+vocab_size = int(len(dictionary))
+
+
+corpus, word_to_id, id_to_word = ptb.load_data('train')
+corpus_val, _, _ = ptb.load_data('val')
+corpus_test, _, _ = ptb.load_data('test')
+
+if config.GPU:
+    corpus = to_gpu(corpus)
+    corpus_val = to_gpu(corpus_val)
+    corpus_test = to_gpu(corpus_test)
+
+vocab_size = len(word_to_id)
+xs = corpus[:-1]
+ts = corpus[1:]
+
+model = BetterRnnlm(vocab_size, wordvec_size, hidden_size, dropout)
+optimizer = SGD(lr)
+trainer = RnnlmTrainer(model, optimizer)
+
+best_ppl = float('inf')
+for epoch in range(max_epoch):
+    trainer.fit(xs, ts, max_epoch=1, batch_size=batch_size,
+                time_size=time_size, max_grad=max_grad)
+
+    model.reset_state()
+    ppl = eval_perplexity(model, corpus_val)
+    print('valid perplexity: ', ppl)
+
+    if best_ppl > ppl:
+        best_ppl = ppl
+        model.save_params()
+    else:
+        lr /= 4.0
+        optimizer.lr = lr
+
+    model.reset_state()
+    print('-' * 50)
+
+
+# テストデータでの評価
+model.reset_state()
+ppl_test = eval_perplexity(model, corpus_test)
+print('test perplexity: ', ppl_test)
